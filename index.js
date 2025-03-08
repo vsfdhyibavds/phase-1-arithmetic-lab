92e10defd5e0cf8912c1cd4cdf6127c938cdcd87
Eugene Wekesa <eugenco578@gmail.com>
2025-03-08 22:53:18 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// multiply
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// random
+const random = Math.floor(Math.random()*100) + 1;
+
+// modulus
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// max
+const max = Math.max(1,5,10,15,20);
